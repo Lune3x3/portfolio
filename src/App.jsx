1b0c3661eaeb1862ed782865ab83c2ed41ba5a6c
Lune3x3 <robsmith.exe@gmail.com>
2025-03-01 22:41:11 -0500

    added basis for functionality and folders

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,92 @@ import { useState } from 'react'
 
 import './App.css'
 
+const handelFunction = (t) => {
+  const lex = t.split(" ");
+  console.log(t);
+  if (lex[0] == "clear") {
+    return (-1);
+  }
+  else if (lex[0] == "help") {
+    return ([
+      "[cat] - concatinates files and prints into shell",
+      "[ls] - lists files and directories",
+      "[clear] - clears shell of all previous lines",
+
+    ])
+  }
+  else {
+    return ([
+      "root@kali:~$ " + t, 
+      lex[0] + " is not a function",
+      "type [help] for a list of all functions"
+    ]);
+  }
+}
+
 function App() {
-  const header = ""
   const [text, setText]  = useState([
-    "Hello world", 
-    "🟦",
-    "fndjakflna \ndnfjkdaslfdsa"
+`
+. . . . . . . . . . . . . . 
+                        . . , ; : c c c , . 
+                    . . . . . . ' ' ' ; l x O . 
+. . . . . ' ' ' ' . . . . . . . . . . , : l d ; 
+                      . ' ; ; ; : : : ; , , . x , 
+            . . ' ' ' .                         0 X x o c : , .     . . . 
+    . . . .                                 , O N k c ; , ; c o k O d c ' , . 
+  .                                       O M o                       ' : d d o . 
+                                          d M c                               : O O ; 
+                                          0 M .                                   . : o . 
+                                          ; W d 
+                                            ; X O , 
+                                                , d 0 O d l c ; , . . 
+                                                        . . ' , ; : c d O O d : : , . 
+                                                                          . : d ; . ' : ; . 
+                                                                                ' d ,     . ' 
+                                                                                    ; l       . . 
+                                                                                      . o 
+                                                                                          c 
+                                                                                          . ' 
+                                                                                            . 
+`,
+"============================================",
+"Enter [help] to see a list of commands",
+"============================================"
   ]);
 
   return (
     <div>
       {text.map(e => {
         return (
-          <div className='entry'>
+          <div className = "entry">
             {e}
           </div>
         )
       })}
 
-      <div>
+      <div className = "input">
         root@kali:~$ 
         <input
+          className = "input"
           type = "text"
           id = "textEnter"
           onKeyDown = {(e) => {
             if (e.key === "Enter") {
-              console.log(document.getElementById("textEnter").value);
-              setText([...text,"root@kali:~$ " + document.getElementById("textEnter").value]);
+              let ret = document.getElementById("textEnter").value;
+
               document.getElementById("textEnter").value = "";
-              document.getElementById("textEnter").scrollIntoView({behavior: "smooth"});
+
+              let render = handelFunction(ret);
+              console.log(render);
+              if (render == -1) {
+                text = setText([]);
+              }
+              else {
+                setText([...text, ...render]);
+              }
+
+
+              document.getElementById("textEnter").scrollIntoView({behavior: "smooth", block: "end"});
             }
           }}
         />
